03d21224adf6ba1e638249674505a85d50773756
Martichou <m@rtin.fyi>
2021-08-09 00:51:34 +0200

    Add tests for sanity of lists (dups, spaces, ...) and fix tests

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -2,11 +2,12 @@ use uuid::Uuid;
 use uuid_readable_rs::{generate, generate_from, generate_inverse, short};
 
 fn main() {
-    let uuid = Uuid::parse_str("29b0c30d-0a73-4036-8d5e-91ac43bca845").unwrap();
+    let uuid = Uuid::parse_str("08c60edc-1297-476c-a876-cef77a014757").unwrap();
     let x = generate_from(uuid);
     println!("Converted uuid = {}", x);
-    println!("Original uuid = {}", &uuid);
-    println!("Decoded uuid = {}\n", generate_inverse(&x).unwrap());
+    println!();
+    println!("Decoded uuid = {}", generate_inverse(&x).unwrap());
+    println!("Original uuid = {}\n", &uuid);
 
     println!("Random full-lenght = {}", generate());
     println!("Random short-lenght = {}", short());
